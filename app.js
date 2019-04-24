edbe099a3bec244f18e46f219419337b4aea5a8d
shatarchi98 <goyal.shatarchi98@gmail.com>
2019-04-24 15:12:47 +0530

    Weather app built on Node

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const hbs = require('hbs');
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/views'));
 app.set('view engine', 'hbs');
@@ -9,5 +10,7 @@ app.set('view engine', 'hbs');
 app.get('/', (req, res)=>{
     res.render("home.hbs");
 });
-app.listen(3000);
+app.listen(port, () => {
+    console.log("Listening on port: ", port);
+});
 
